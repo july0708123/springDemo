32976de4f95fea7bd5d4fd736751f35c54935fa2
july0708123 <a15602029078@163.com>
2022-07-16 15:13:37 +0800

    哈夫曼树

--- src/main/java/com/demo/algorithm/HuffmanTree.java
+++ src/main/java/com/demo/algorithm/HuffmanTree.java
@@ -69,11 +69,69 @@ public class HuffmanTree {
         }
     }
 
-    Map<String,String> encryCode(){
+    /**
+     * 创建密码本
+     * @return
+     */
+    Map<String,String> secretBook(){
         Map<String,String> map = new HashMap<>();
-
+        for(HuffmanNode leaf:leafs){
+            String val = leaf.chars;
+            String code = "";
+            while (leaf.parent != null){
+                if(leaf.parent.left == leaf){
+                    code = "0" + code;
+                }else{
+                    code = "1" + code;
+                }
+                leaf = leaf.parent;
+            }
+            System.out.println(val+"："+code);
+            map.put(val,code);
+        }
         return map;
     }
+
+    /**
+     * 加密
+     * @param info
+     * @param book
+     * @return
+     */
+    String encodeInfo(String info,Map<String,String> book){
+        String sc = "";
+        for(int i=0;i<info.length();i++){
+            sc = sc + book.get(""+info.charAt(i));
+        }
+        return sc;
+    }
+
+    /**
+     * 解密
+     * @param secret
+     * @param book
+     * @return
+     */
+    String decodeSecret(String secret,Map<String,String> book){
+        String str = "";
+        Map<String,String> booknew = new HashMap<>(); //将密码本的key,value换个位置，方便查找
+        Set<String> set = book.keySet();
+        for(String k:set){
+            booknew.put(book.get(k),k);
+        }
+        for(int i=0;i<secret.length();i++){
+            for(int j=i+1;j<=secret.length();j++){
+                String c = secret.substring(i,j);
+                if(booknew.containsKey(c)){
+                    str = str + booknew.get(c);
+                    i = j-1;
+                    break;
+                }
+            }
+        }
+        return str;
+    }
+
 
     public static void main(String[] args) {
 //        String string = "abbbacd";
@@ -87,6 +145,13 @@ public class HuffmanTree {
         tree.wtMap.put("f", 4);
         tree.wtMap.put("g", 12);
         tree.createHuffmanTree(); //建树
+        Map<String,String> book = tree.secretBook(); //建立密码本
+
+        String secret = tree.encodeInfo("ddgecaf",book);
+        System.out.println("加密后："+secret);
+
+        String info = tree.decodeSecret(secret,book);
+        System.out.println("解密后："+info);
 
         System.out.println("=========");
     }
