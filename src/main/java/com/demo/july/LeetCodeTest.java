bb03a22508ff5b3dedb416a51757d760e3b76681
july0708123 <a15602029078@163.com>
2022-08-05 19:36:19 +0800

    测试单文件revert

--- src/main/java/com/demo/july/LeetCodeTest.java
+++ src/main/java/com/demo/july/LeetCodeTest.java
@@ -1,14 +1,26 @@
 package com.demo.july;
 
+import org.springframework.util.StringUtils;
+import sun.misc.Unsafe;
 import sun.reflect.generics.tree.Tree;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class LeetCodeTest {
 
+    public static void main(String[] args) {
+        int n = 6279;
+        System.out.println(reverseBits(n));
+    }
+
+    public static int reverseBits(int i) {
+        i = (i & 0x55555555) << 1 | (i >>> 1) & 0x55555555;
+        i = (i & 0x33333333) << 2 | (i >>> 2) & 0x33333333;
+        i = (i & 0x0f0f0f0f) << 4 | (i >>> 4) & 0x0f0f0f0f;
+        i = (i << 24) | ((i & 0xff00) << 8) |
+                ((i >>> 8) & 0xff00) | (i >>> 24);
+        return i;
+    }
 }
 
 
